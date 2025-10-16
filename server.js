88e88d11e0093d15fd9226d1b83050255ab6ebb8
gauravtrs <gauravpateltrs@gmail.com>
2025-10-16 12:44:48 +0530

    changes in server.js file

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(cors({
 
 app.use(express.json());
 
-//set cookie-parser middleware
+//set cookie-parser middleware for cookies
 app.use(cookieParser());
 
 app.use(passport.initialize());
